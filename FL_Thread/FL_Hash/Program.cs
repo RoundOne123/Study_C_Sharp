9589c63a4c31442151b94272ca79d2d2d1cee326
RoundOne123 <2608641086@qq.com>
2019-03-12 10:20:28 +0800

    add notes

---
+++ FL_Thread/FL_Hash/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace FL_Hash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = Hashtable.Synchronized(new Hashtable());
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+
+            HashSet<string> hs = new HashSet<string>();
+            ArrayList al = new ArrayList();
+            List<int> list = new List<int>();
+            Console.WriteLine(list.Capacity);
+            list.Add(1);
+            Console.WriteLine(list.Capacity);
+
+            Console.ReadLine();
+        }
+
+
+
+
+    }
+}
